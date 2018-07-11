65055703cfdcda8bc719ca64b19234822a8f859b
htf <2889921390@qq.com>
2018-07-11 14:41:35 +0800

    第三次提交

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,7 +2,18 @@
  * Created by Administrator on 2018/7/11.
  */
 public class Test {
+
+    private String password;
+
     private String name;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String getName() {
         return name;
